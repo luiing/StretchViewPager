cfd1b22bbaafa63714f4ed1b15a09dba5b3f3a54
uis <huaibing.liu@bl.com>
2018-07-25 15:10:25 +0800

    fixed refresh done

--- stretch/src/main/java/com/uis/stretch/OnStretchListener.java
+++ stretch/src/main/java/com/uis/stretch/OnStretchListener.java
@@ -5,19 +5,24 @@ package com.uis.stretch;
  */
 public interface OnStretchListener {
     /**
-     * @param direction 拽拉方向, STRETCH_LEFT 左边拽拉，STRETCH_RIGHT 右边拽拉
+     * scrolling
+     * @param direction model one of {@link StretchPager#STRETCH_RIGHT},{@link StretchPager#STRETCH_RIGHT};
+     *                 拽拉方向, STRETCH_LEFT 左边拽拉，STRETCH_RIGHT 右边拽拉
      * @param distance 滑动方向：x GT 0 is move right,x LT 0 is move left(x大于0向右，小于0向左)
      */
     void onScrolled(int direction,int distance);
 
     /**
-     * @param direction 拽拉方向, STRETCH_LEFT 左边拽拉，STRETCH_RIGHT 右边拽拉
+     * 正在刷新 refreshing
+     * @param direction model one of {@link StretchPager#STRETCH_RIGHT},{@link StretchPager#STRETCH_RIGHT};
+     *                  拽拉方向, STRETCH_LEFT 左边拽拉，STRETCH_RIGHT 右边拽拉
      * @param distance 释放时偏移量，为正数abs
      */
     void onRefresh(int direction,int distance);
 
     /**
-     * 回弹结束
+     * 回弹结束 end
+     * @param direction model one of {@link StretchPager#STRETCH_RIGHT},{@link StretchPager#STRETCH_RIGHT};
      */
     void onRelease(int direction);
 }
---
+++ stretch/src/main/java/com/uis/stretch/SimpleStretchListener.java
@@ -0,0 +1,21 @@
+package com.uis.stretch;
+
+/**
+ * @author uis on 2018/7/24.
+ */
+public class SimpleStretchListener implements OnStretchListener {
+    @Override
+    public void onScrolled(int direction, int distance) {
+
+    }
+
+    @Override
+    public void onRefresh(int direction, int distance) {
+
+    }
+
+    @Override
+    public void onRelease(int direction) {
+
+    }
+}
